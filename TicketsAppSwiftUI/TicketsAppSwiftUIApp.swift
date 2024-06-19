ac21ad709bf8d21049d5cd128cb428155bbba8a5
Aleksey Shepelev <aleksey.shepelev@scompler.com>
2024-06-19 16:15:46 +0400

    Initial Commit

---
+++ TicketsAppSwiftUI/TicketsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketsAppSwiftUIApp.swift
+//  TicketsAppSwiftUI
+//
+//  Created by Aleksey Shepelev on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicketsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
